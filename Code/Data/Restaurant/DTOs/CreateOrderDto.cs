d48b0664c23430566b2510aeaff0e04a0f6e4693
MaksimPtrv <maksip@taltech.ee>
2026-05-26 19:41:39 +0300

    CreateOrderDto changed

--- Code/Data/Restaurant/DTOs/CreateOrderDto.cs
+++ Code/Data/Restaurant/DTOs/CreateOrderDto.cs
@@ -3,5 +3,8 @@ namespace Data.Restaurant.DTOs;
 public class CreateOrderDto
 {
     public Guid CustomerId { get; set; }
+
+    public Guid? ReservationId { get; set; }
+
     public List<CreateOrderItemDto> Items { get; set; } = new();
 }
