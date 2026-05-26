5caa36e51a21817562dfaebc5fc53fb7c62268ee
bogdantwelve <bogdantwelve@gmail.com>
2026-05-26 18:09:41 +0300

    add payment page

--- Soft/RestoranBookingApp/obj/Debug/net10.0/RestoranBookingApp.AssemblyInfo.cs
+++ Soft/RestoranBookingApp/obj/Debug/net10.0/RestoranBookingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestoranBookingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8f055dfe8f139c7573175d8029fea4f2251b1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a338ae53bfa853fffcf741d13394869b80eab7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestoranBookingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestoranBookingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
