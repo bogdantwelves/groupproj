c9e3777283515841794504890d1f2226d152a869
MaksimPtrv <maksip@taltech.ee>
2026-05-26 19:32:00 +0300

    OrderItem changed

--- Code/Data/Restaurant/Entities/OrderItem.cs
+++ Code/Data/Restaurant/Entities/OrderItem.cs
@@ -5,9 +5,11 @@ public class OrderItem
     public Guid Id { get; set; }
 
     public Guid OrderId { get; set; }
+
     public Order? Order { get; set; }
 
     public Guid MenuItemId { get; set; }
+
     public MenuItem? MenuItem { get; set; }
 
     public int Quantity { get; set; }
