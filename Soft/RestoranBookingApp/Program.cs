7a338ae53bfa853fffcf741d13394869b80eab7b
bogdantwelve <bogdantwelve@gmail.com>
2026-05-26 18:08:15 +0300

    register payment and dashboard services

--- Soft/RestoranBookingApp/Program.cs
+++ Soft/RestoranBookingApp/Program.cs
@@ -14,6 +14,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddScoped<IAdminDashboardService, AdminDashboardService>();
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
