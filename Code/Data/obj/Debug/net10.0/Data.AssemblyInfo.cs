2e12582ac4ed55761c4151ffcee026e3d202333a
RobertZadoroznoi <rzador@taltech.ee>
2026-05-26 16:56:24 +0300

    add Ingridient dto, inventory dto, table dto

--- Code/Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
+++ Code/Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8f055dfe8f139c7573175d8029fea4f2251b1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d885b48d3231dcbd883f27298695b7671f9391bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
