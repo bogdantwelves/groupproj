3c2d4e70a89668daa8ec4213edf65119736f800f
MaksimPtrv <maksip@taltech.ee>
2026-05-26 19:20:53 +0300

    menu.cs code fixed

--- Code/Data/Restaurant/Entities/Menu.cs
+++ Code/Data/Restaurant/Entities/Menu.cs
@@ -3,7 +3,12 @@ namespace Data.Restaurant.Entities;
 public class Menu
 {
     public Guid Id { get; set; }
+
     public string Name { get; set; } = string.Empty;
+
+    public Guid RestaurantId { get; set; }
+
+    public Restaurant? Restaurant { get; set; }
 
     public List<MenuItem> Items { get; set; } = new();
 }
