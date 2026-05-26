2691b063eedf713fc18a2a262343fb22a459e49e
MaksimPtrv <maksip@taltech.ee>
2026-05-26 19:31:19 +0300

    Order.cs changed

---
+++ Code/Data/Restaurant/Entities/Order.cs
@@ -0,0 +1,24 @@
+namespace Data.Restaurant.Entities;
+
+public class Order
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public OrderStatus Status { get; set; } = OrderStatus.Created;
+
+    public Guid CustomerId { get; set; }
+
+    public Customer? Customer { get; set; }
+
+    public Guid? ReservationId { get; set; }
+
+    public Reservation? Reservation { get; set; }
+
+    public List<OrderItem> Items { get; set; } = new();
+
+    public Payment? Payment { get; set; }
+
+    public decimal TotalAmount => Items.Sum(x => x.UnitPrice * x.Quantity);
+}
