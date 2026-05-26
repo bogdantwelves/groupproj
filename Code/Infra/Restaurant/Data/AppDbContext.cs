81be9b74c25c727839831a62a6a0627ca492abb8
nikitasinilill <nikisi@taltech.ee>
2026-05-26 19:52:58 +0300

    AppDbContext.cs change

--- Code/Infra/Restaurant/Data/AppDbContext.cs
+++ Code/Infra/Restaurant/Data/AppDbContext.cs
@@ -10,28 +10,55 @@ public class AppDbContext : DbContext
     {
     }
 
-    // Person 1: Reservations
+    public DbSet<Restaurant> Restaurants => Set<Restaurant>();
+
+    public DbSet<RestaurantTable> Tables => Set<RestaurantTable>();
+
     public DbSet<Customer> Customers => Set<Customer>();
+
+    public DbSet<Staff> Staff => Set<Staff>();
+
+    public DbSet<Menu> Menus => Set<Menu>();
+
+    public DbSet<MenuItem> MenuItems => Set<MenuItem>();
+
     public DbSet<Reservation> Reservations => Set<Reservation>();
 
-    // Person 2: Menu and Orders
-    public DbSet<Menu> Menus => Set<Menu>();
-    public DbSet<MenuItem> MenuItems => Set<MenuItem>();
     public DbSet<Order> Orders => Set<Order>();
+
     public DbSet<OrderItem> OrderItems => Set<OrderItem>();
 
-    // Person 3: Payments and Admin
+    public DbSet<Payment> Payments => Set<Payment>();
 
-    // Person 4: Tables and Inventory
-    public DbSet<Restaurant> Restaurants => Set<Restaurant>();
-    public DbSet<RestaurantTable> Tables => Set<RestaurantTable>();
     public DbSet<Inventory> Inventories => Set<Inventory>();
+
     public DbSet<Ingredient> Ingredients => Set<Ingredient>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        // Feature-specific configuration will be added here.
+        modelBuilder.Entity<MenuItem>()
+            .Property(x => x.Price)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<OrderItem>()
+            .Property(x => x.UnitPrice)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Payment>()
+            .Property(x => x.Amount)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Ingredient>()
+            .Property(x => x.Quantity)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Ingredient>()
+            .Property(x => x.LowStockThreshold)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Order>()
+            .Ignore(x => x.TotalAmount);
     }
 }
